b1214338e0cf9e1c2dc9b23a8116ed68d5f51f32
Darix SAMANI SIEWE <samanidarix@gmail.com>
2026-03-12 14:18:38 +0100

    schemas boby to add triple

---
+++ src/schemas/triples.rs
@@ -0,0 +1,11 @@
+
+
+use salvo_oapi::ToSchema;
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize, Debug, ToSchema)]
+pub struct TripleInput {
+    pub object: String,
+    pub predicate: String,
+    pub subject: String,
+}
