e13eb9ee84bc1b3c262ebd100fecf6a41abaeb44
Darix SAMANI SIEWE <samanidarix@gmail.com>
2026-03-14 15:23:37 +0100

    add graphs endpoints

--- src/router.rs
+++ src/router.rs
@@ -1,4 +1,5 @@
 use salvo::Router;
+use crate::api::graph::{create_graph, delete_graph, get_graphs};
 use crate::api::triple::add_triple;
 use crate::api::sparql::{sparql, sparql_get};
 
@@ -6,6 +7,12 @@ use crate::api::sparql::{sparql, sparql_get};
 pub fn api_router () -> Router{
     let router = Router::new()
                         .push(Router::with_path("triples").post(add_triple))
-                        .push(Router::with_path("sparql").post(sparql).get(sparql_get));
+                        .push(Router::with_path("sparql").post(sparql).get(sparql_get))
+                        .push(
+                            Router::with_path("graphs")
+                            .post(create_graph)
+                            .delete(delete_graph)
+                            .get(get_graphs)
+                        );
     router
 }
